4704dd15683e6dfccb9f3f9204f3ed45dc31f7ed
Evgeniy Antonovich <gnoptswbn5kj@mail.ru>
2023-07-03 22:49:59 +0300

    feat: done ws03

--- app/src/main/java/com/android/fundamentals/workshop03/task/WS03AssignmentFragment.kt
+++ app/src/main/java/com/android/fundamentals/workshop03/task/WS03AssignmentFragment.kt
@@ -8,12 +8,15 @@ import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.android.fundamentals.R
 
+
 //TODO(WS2:1) Create fragment class, extend Fragment
 class WS03AssignmentFragment : Fragment() {
 
     private var btnIncrement: Button? = null
     private var btnChangeBackground: Button? = null
+
     //TODO(WS2:5) Create a variable ClickListener
+    private var listener: ClickListener? = null
 
     //TODO(WS2:2) Override onCreateView() method
     override fun onCreateView(
@@ -26,14 +29,23 @@ class WS03AssignmentFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         btnIncrement = view.findViewById<Button>(R.id.btn_increment).apply {
             //TODO(WS2:7) Set button OnClickListener
+            setOnClickListener { listener?.increaseValue() }
         }
         btnChangeBackground = view.findViewById<Button>(R.id.btn_change_background).apply {
             //TODO(WS2:8) Set button OnClickListener
+            setOnClickListener { listener?.changeBackground() }
         }
 
     }
 
     //TODO(WS2:6) Create fun setListener() initialize clickListener in Fragment
+    fun setListener(l: ClickListener) {
+        listener = l
+    }
 
     //TODO(WS2:4) Create interface ClickListener
+    interface ClickListener {
+        fun increaseValue()
+        fun changeBackground()
+    }
 }
